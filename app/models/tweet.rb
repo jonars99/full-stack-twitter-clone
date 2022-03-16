30b6c970c711652fbcc3307fb6f07dfddca14454
Jo <88668777+jonars99@users.noreply.github.com>
2022-03-16 15:13:30 +0000

    update model validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ApplicationRecord
+  belongs_to :user
+
+  validates :message, length: { maximum: 140 }
+  validates :user_id, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum: 64 }
-  validates :password, presence: true, length: { minimum: 8, maximum: 64 }
-  validates :email, presence: true, length: { minimum: 5, maximum: 500 }
+  has_many :tweets
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 64  }
+  validates :password, presence: true, length: { minimum: 8, maximum: 64  }
+  validates :email,    presence: true, length: { minimum: 5, maximum: 500 }
 
   validates_uniqueness_of :username 
   validates_uniqueness_of :email 
