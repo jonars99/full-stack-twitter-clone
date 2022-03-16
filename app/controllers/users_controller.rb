778e0346b272165fe178868d718359722a696b53
Jo <88668777+jonars99@users.noreply.github.com>
2022-03-16 16:48:25 +0000

    define post method to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save 
+      render json: {
+        user: {
+          username: @user.username
+        }
+      }
+    else 
+      render json: {
+        success: false
+      }
+    end
+  end
+
+  private
+
+  def user_params 
+    params.requre(:user).permit(:username, :password)
+  end
+
 end
