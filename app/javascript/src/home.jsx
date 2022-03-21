d1a5ae64150b5c9e113da153ef9467c3b1aaddd1
Jo <88668777+jonars99@users.noreply.github.com>
2022-03-21 11:29:14 +0000

    change for to htmlFor for correct jsx

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -6,15 +6,15 @@ const Home = () => (
     <div className="col-6 m-auto my-5">
       <form>
         <div>
-          <label for="usernameInput" className="form-label">Username</label>
+          <label htmlFor="usernameInput" className="form-label">Username</label>
           <input type="text" className="form-control" id="usernameInput" placeholder="username"></input>
         </div>
         <div>
-          <label for="emailInput" className="form-label">Email adress</label>
+          <label htmlFor="emailInput" className="form-label">Email adress</label>
           <input type="email" className="form-control" id="emailInput" placeholder="email"></input>
         </div>
         <div>
-          <label for="passwordInput" className="form-label">Password</label>
+          <label htmlFor="passwordInput" className="form-label">Password</label>
           <input type="text" className="form-control" id="passwordInput" placeholder="password"></input>
         </div>
         <button type="submit" className="btn signup-btn">Sign up</button>
