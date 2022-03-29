8b0e82bcd36dcceb4963f5bef61f2b3415368e1b
Jo <88668777+jonars99@users.noreply.github.com>
2022-03-29 16:02:59 +0100

    change twitter_token to twitter_session_token

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,51 +1,52 @@
-class SessionsController < ApplicationController
+module Api
+  class SessionsController < ApplicationController
 
-  def create
-    @user = User.find_by(username: params[:user][:username])
+    def create
+      @user = User.find_by(username: params[:user][:username])
 
-    if @user and BCrypt::Password.new(@user.password) == params[:user][:password]
-      session = @user.sessions.create
-      cookies.permanent.signed[:twitter_token] = {
-        value: session.token,
-        httponly: true
-      }
-      render json: { success: true }
+      if @user and BCrypt::Password.new(@user.password) == params[:user][:password]
+        session = @user.sessions.create
+        cookies.permanent.signed[:twitter_session_token] = {
+          value: session.token,
+          httponly: true
+        }
+        render json: { success: true }
+      else
+        render json: { success: false }
+        puts @user.errors.to_yaml
+      end
 
-    else
-      render json: { success: false }
+    end
+
+    def authenticated
+      token = cookies.permanent.signed[:twitter_session_token]
+      session = Session.find_by(token: token)
+
+      if session 
+        user = session.user 
+
+        render json: {
+          authenticated: true,
+          username: user.username
+        }
+
+      else 
+        render json: {
+          authenticated: false
+        }
+      end
+
+    end
+
+    def destroy
+      token = cookies.permanent.signed[:twitter_session_token]
+      session = Session.find_by[token: token]
+
+      if session and session.destroy
+        render json: { success: true }
+      end
 
     end
 
   end
-
-  def authenticated
-    token = cookies.permanent.signed[:twitter_token]
-    session = Session.find_by(token: token)
-
-    if session 
-      user = session.user 
-
-      render json: {
-        authenticated: true,
-        username: user.username
-      }
-
-    else 
-      render json: {
-        authenticated: false
-      }
-    end
-
-  end
-
-  def destroy
-    token = cookies.permanent.signed[:twitter_token]
-    session = Session.find_by[token: token]
-
-    if session and session.destroy
-      render json: { success: true }
-    end
-
-  end
-
 end
