9bbcfc4b4ff5b6cceb4b853539646d3ec8b2382d
Jo <88668777+jonars99@users.noreply.github.com>
2022-04-05 14:48:39 +0100

    style homepage (log in/sign up)

--- app/javascript/src/Home.jsx
+++ app/javascript/src/Home.jsx
@@ -2,12 +2,18 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import { createUser, logInUser, authenticateUser } from '../packs/requests';
+import './stylesheets/styles';
+import image from './images/pexels-humphrey-muleba-1647121.jpg';
 
 const Home = () => {
+
+  //   states
 
   const [signUpMessage, setSignUpMessage] = useState("");
   const [logInMessage, setLogInMessage] = useState("");
   
+  //   handlers
+
   const handleSignUp = (event) => {
     event.preventDefault();
     const username = $('#newUsernameInput').val();
@@ -37,6 +43,8 @@ const Home = () => {
     });
   }
 
+  //  go to feed if user is logged in
+
   useEffect(() => {
     authenticateUser(function(response) {
       if (response.authenticated == true) {
@@ -46,47 +54,71 @@ const Home = () => {
   }, [])
 
   return(
-    <React.Fragment>
-      <div className="col-10 m-auto my-5">
+    <div id="homePage" className="container-fluid">
+      <div className="row">
 
-        <form onSubmit={handleSignUp} className="border">
-        <p>Sign Up</p>
-          <div>
-            <label htmlFor="newUsernameInput" className="form-label">Username</label>
-            <input type="text" className="form-control" id="newUsernameInput" placeholder="username" minLength="3" required></input>
-          </div>
-          <div>
-            <label htmlFor="newEmailInput" className="form-label">Email adress</label>
-            <input type="email" className="form-control" id="newEmailInput" placeholder="email" required></input>
-          </div>
-          <div>
-            <label htmlFor="newPasswordInput" className="form-label">Password</label>
-            <input type="password" className="form-control" id="newPasswordInput" placeholder="password" minLength="8" required></input>
-          </div>
-          <button type="submit" className="btn sign-up" onSubmit={handleSignUp}>Sign up</button>
-          <p>
-            {signUpMessage}
-          </p>
-        </form>
+        <div id="homeLeft" className="col-6 d-none d-lg-flex px-0">
+          <img className="img-fluid p-4 ps-0" src={image} alt="graffiti wall art image"></img>
+        </div>
 
-        <form onSubmit={handleLogIn} className="border">
-          <p>Log In</p>
-          <div>
-            <label htmlFor="usernameInput" className="form-label">Username</label>
-            <input type="text" className="form-control" id="usernameInput" placeholder="username" required></input>
-          </div>
-          <div>
-            <label htmlFor="passwordInput" className="form-label">Password</label>
-            <input type="password" className="form-control" id="passwordInput" placeholder="password" required></input>
-          </div>
-          <button type="submit" className="btn log-in" onSubmit={handleLogIn}>Log In</button>
-          <p>
-            {logInMessage}
-          </p>
-        </form>
+        <div className="col-12 col-lg-6 fw-bold p-4 home-wrapper">
 
+          <div className="twitter-icon text-center">
+            <i className="fa-brands fa-twitter"></i>
+          </div>
+          <div className="my-3 mb-5 mb-lg-3">
+            <h1 className="my-3">Happening Now</h1>
+            <h3>Join Twitter today.</h3>
+          </div>
+
+          <div className="row flex-column flex-sm-row text-center">
+
+            <div className="col-12 col-sm-6 d-flex">
+              <form onSubmit={handleSignUp} className="home-page-forms d-flex flex-column justify-content-around w-100 p-4">
+                <p className="heading">Create your account</p>
+                <div>
+                  <label htmlFor="newUsernameInput" className="form-label" hidden>Username</label>
+                  <input type="text" className="form-control" id="newUsernameInput" placeholder="username" minLength="3" required></input>
+                </div>
+                <div>
+                  <label htmlFor="newEmailInput" className="form-label" hidden>Email adress</label>
+                  <input type="email" className="form-control" id="newEmailInput" placeholder="email" required></input>
+                </div>
+                <div>
+                  <label htmlFor="newPasswordInput" className="form-label" hidden>Password</label>
+                  <input type="password" className="form-control" id="newPasswordInput" placeholder="password" minLength="8" required></input>
+                </div>
+                <button type="submit" className="btn mt-3" onSubmit={handleSignUp}>Sign up</button>
+                <p>
+                  {signUpMessage}
+                </p>
+              </form>
+            </div>
+
+            <div className="col-12 col-sm-6 d-flex my-4 my-sm-0">
+              <form onSubmit={handleLogIn} className="home-page-forms d-flex flex-column justify-content-around w-100 p-4">
+                <p className="heading">Already have an account?</p>
+                <div>
+                  <label htmlFor="usernameInput" className="form-label" hidden>Username</label>
+                  <input type="text" className="form-control" id="usernameInput" placeholder="username" required></input>
+                </div>
+                <div>
+                  <label htmlFor="passwordInput" className="form-label" hidden>Password</label>
+                  <input type="password" className="form-control mb-5 mb-sm-3" id="passwordInput" placeholder="password" required></input>
+                </div>
+                <button type="submit" className="btn mt-3" onSubmit={handleLogIn}>Log In</button>
+                <p>
+                  {logInMessage}
+                </p>
+              </form>
+            </div>
+
+          </div>
+
+        </div>
+        
       </div>
-    </React.Fragment>
+    </div>
   )
 }
 
